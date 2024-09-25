0e75c1185986c20c803b669239699fb5ad26e989
Mark <marcgimdi@gmail.com>
2024-09-25 13:28:18 +0200

    Artistas marciales y escuelas a medio hacer

--- BBDD/mysql/crud.py
+++ BBDD/mysql/crud.py
@@ -7,15 +7,14 @@ from .models import ArtistaMarcial, Escuela
 
 
 # TODO ARTISTAS MARCIALES
+
 def get_artista_marcial_by_dni(db: Session, dni: str):
-    # Consulta para obtener el artista por su DNI
+    """Obtiene un artista marcial por su DNI."""
     artista = db.query(ArtistaMarcial).filter(ArtistaMarcial.dni == dni).first()
 
-    # Si no se encuentra el artista, devolver None
     if not artista:
         return None
 
-    # Retornar un diccionario con todos los campos menos la contraseña
     return {
         "id": artista.id,
         "dni": artista.dni,
@@ -32,14 +31,14 @@ def get_artista_marcial_by_dni(db: Session, dni: str):
 
 
 def artista_marcial_exists(db: Session, dni: str) -> bool:
+    """Verifica si un artista marcial existe por su DNI."""
     return db.query(ArtistaMarcial).filter(ArtistaMarcial.dni == dni).first() is not None
 
 
 def create_artista_marcial(db: Session, artista: ArtistaMarcialCreate):
-    # Hashear la contraseña
+    """Crea un nuevo artista marcial."""
     hashed_password = PasswordEncryptor.hash_password(artista.contrasena)
 
-    # Creamos una instancia del modelo ArtistaMarcial
     db_artista = ArtistaMarcial(
         dni=artista.dni,
         nombre=artista.nombre,
@@ -51,60 +50,52 @@ def create_artista_marcial(db: Session, artista: ArtistaMarcialCreate):
         escuela_id=artista.escuela_id,
         cinturon=artista.cinturon,
         grado=artista.grado,
-        contrasena=hashed_password  # Guardamos la contraseña hasheada
+        contrasena=hashed_password
     )
 
-    # Añadimos el nuevo artista a la sesión de la base de datos
     db.add(db_artista)
-    db.commit()  # Confirmamos la transacción
-    db.refresh(db_artista)  # Refrescamos la instancia para obtener los datos actualizados
+    db.commit()
+    db.refresh(db_artista)
 
     return db_artista
 
 
 def delete_artista_marcial_by_dni(db: Session, dni: str):
-    # Buscar el artista por DNI
+    """Elimina un artista marcial por su DNI."""
     artista = db.query(ArtistaMarcial).filter(ArtistaMarcial.dni == dni).first()
 
-    # Si no se encuentra, lanzar una excepción
     if not artista:
         raise HTTPException(status_code=404, detail="Artista marcial no encontrado")
 
-    # Eliminar el artista
     db.delete(artista)
-    db.commit()  # Confirmar la eliminación
+    db.commit()
     return {"detail": "Artista marcial eliminado exitosamente"}
 
 
 def delete_artista_marcial_by_id(db: Session, artista_id: int):
-    # Buscar el artista por ID
+    """Elimina un artista marcial por su ID."""
     artista = db.query(ArtistaMarcial).filter(ArtistaMarcial.id == artista_id).first()
 
-    # Si no se encuentra, lanzar una excepción
     if not artista:
         raise HTTPException(status_code=404, detail="Artista marcial no encontrado")
 
-    # Eliminar el artista
     db.delete(artista)
-    db.commit()  # Confirmar la eliminación
+    db.commit()
     return {"detail": "Artista marcial eliminado exitosamente"}
 
 
 def update_password_by_dni(db: Session, dni: str, new_password: str):
-    # Buscar el artista por su DNI
+    """Actualiza la contraseña de un artista marcial por su DNI."""
     artista = db.query(ArtistaMarcial).filter(ArtistaMarcial.dni == dni).first()
 
     if not artista:
         raise HTTPException(status_code=404, detail="Artista marcial no encontrado")
 
-    # Encriptar la nueva contraseña
     hashed_password = PasswordEncryptor.hash_password(new_password)
-
-    # Actualizar la contraseña
     artista.contrasena = hashed_password
 
-    db.commit()  # Confirmamos los cambios
-    db.refresh(artista)  # Refrescamos la instancia para obtener los datos actualizados
+    db.commit()
+    db.refresh(artista)
 
     return artista
 
@@ -112,17 +103,17 @@ def update_password_by_dni(db: Session, dni: str, new_password: str):
 # TODO ESCUELA
 
 def get_all_escuelas(db: Session):
-    # Consulta para obtener todas las escuelas
+    """Obtiene todas las escuelas."""
     return db.query(Escuela).all()
 
 
 def get_escuela_by_id(db: Session, escuela_id: int):
-    # Consulta para obtener una escuela por su ID
+    """Obtiene una escuela por su ID."""
     return db.query(Escuela).filter(Escuela.id == escuela_id).first()
 
 
 def create_escuela(db: Session, escuela: EscuelaCreate):
-    # Crear una instancia de Escuela a partir del esquema recibido
+    """Crea una nueva escuela."""
     nueva_escuela = Escuela(
         nombre=escuela.nombre,
         direccion=escuela.direccion,
@@ -130,23 +121,20 @@ def create_escuela(db: Session, escuela: EscuelaCreate):
         pais=escuela.pais
     )
 
-    # Añadir la nueva escuela a la base de datos
     db.add(nueva_escuela)
-    db.commit()  # Confirmar los cambios
-    db.refresh(nueva_escuela)  # Refrescar la instancia para obtener el ID generado
+    db.commit()
+    db.refresh(nueva_escuela)
 
     return nueva_escuela
 
 
 def delete_escuela_by_id(db: Session, escuela_id: int):
-    # Buscar la escuela por ID
+    """Elimina una escuela por su ID."""
     escuela = db.query(Escuela).filter(Escuela.id == escuela_id).first()
 
-    # Si no se encuentra, lanzar una excepción
     if not escuela:
         raise HTTPException(status_code=404, detail="Escuela no encontrada")
 
-    # Eliminar la escuela
     db.delete(escuela)
-    db.commit()  # Confirmar la eliminación
+    db.commit()
     return {"detail": "Escuela eliminada exitosamente"}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from typing import List
 
-from fastapi import Depends, Request, Response, FastAPI
-from sqlalchemy.orm import Session  # Asegúrate de importar desde sqlalchemy.orm
+from fastapi import Depends
+# Request, Response, FastAPI
+from sqlalchemy.orm import Session
 from fastapi import FastAPI, HTTPException
 
-from BBDD.mysql import crud, schemas
+from BBDD.mysql import schemas
+# crud
 from BBDD.mysql.crud import get_artista_marcial_by_dni, artista_marcial_exists, create_artista_marcial, \
     get_all_escuelas, get_escuela_by_id, create_escuela, delete_artista_marcial_by_dni, delete_artista_marcial_by_id, \
     delete_escuela_by_id, update_password_by_dni
@@ -28,21 +30,20 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
-        print("Hello World")
     finally:
         db.close()
 
 
-# TODO ARTISTAS MARCIALES
-@app.get("/artistas")
-def read_artistas(db: Session = Depends(get_db)):
-    # Aquí iría tu lógica para consultar la base de datos
-    return {"artistas": "Aquí irían los datos de los artistas"}
+# # TODO ARTISTAS MARCIALES
+# @app.get("/artistas")
+# def read_artistas(db: Session = Depends(get_db)):
+#     # Obtener: Consulta para obtener todos los artistas marciales
+#     return {"artistas": "Aquí irían los datos de los artistas"}
 
 
 @app.get("/artistas-marciales/{dni}", response_model=schemas.ArtistaMarcialInDB)
 def read_artista_marcial(dni: str, db: Session = Depends(get_db)):
-    # Llamada al método CRUD para obtener el artista por DNI
+    # Obtener: Llamada al método CRUD para obtener el artista por DNI
     artista = get_artista_marcial_by_dni(db, dni)
 
     if artista is None:
@@ -53,43 +54,40 @@ def read_artista_marcial(dni: str, db: Session = Depends(get_db)):
 
 @app.get("/artistas-marciales/existe/{dni}", response_model=bool)
 def check_artista_marcial_exists(dni: str, db: Session = Depends(get_db)):
-    # Llamada al método CRUD para verificar si existe
-    if artista_marcial_exists(db, dni):
-        return True
-    return False
+    # Verificar: Llamada al método CRUD para verificar si el artista marcial existe por DNI
+    return artista_marcial_exists(db, dni)
 
 
 @app.post("/artistas-marciales/", response_model=ArtistaMarcialInDB)
 def create_artista(artista: ArtistaMarcialCreate, db: Session = Depends(get_db)):
-    # Verificar si el artista ya existe por DNI
+    # Crear: Crear un nuevo artista marcial
     artista_existente = db.query(ArtistaMarcial).filter(ArtistaMarcial.dni == artista.dni).first()
 
-    if artista_existente:  # Si existe un artista con el mismo dni
+    if artista_existente:
         raise HTTPException(status_code=400, detail="El artista marcial con este DNI ya existe")
 
-    # Crear el artista marcial
     nuevo_artista = create_artista_marcial(db, artista)
-
     return nuevo_artista
 
 
 # Eliminar artista por DNI
 @app.delete("/artistas-marciales/dni/{dni}")
 def delete_artista_marcial_dni(dni: str, db: Session = Depends(get_db)):
+    # Eliminar: Eliminar un artista marcial por su DNI
     return delete_artista_marcial_by_dni(db, dni)
 
 
 # Eliminar artista por ID
 @app.delete("/artistas-marciales/id/{artista_id}")
 def delete_artista_marcial_id(artista_id: int, db: Session = Depends(get_db)):
+    # Eliminar: Eliminar un artista marcial por su ID
     return delete_artista_marcial_by_id(db, artista_id)
 
 
 @app.put("/artistas-marciales/{dni}/contrasena")
 def update_artista_contrasena(dni: str, new_password: str, db: Session = Depends(get_db)):
-    # Llamamos al método CRUD para actualizar la contraseña
+    # Actualizar: Actualizar la contraseña de un artista marcial por su DNI
     artista_actualizado = update_password_by_dni(db, dni, new_password)
-
     return {"message": "Contraseña actualizada correctamente", "artista": artista_actualizado}
 
 
@@ -97,20 +95,18 @@ def update_artista_contrasena(dni: str, new_password: str, db: Session = Depends
 
 @app.get("/escuelas", response_model=List[EscuelaInDB])
 def read_escuelas(db: Session = Depends(get_db)):
-    # Llamar al método CRUD para obtener todas las escuelas
+    # Obtener: Llamar al método CRUD para obtener todas las escuelas
     escuelas = get_all_escuelas(db)
     if not escuelas:
         raise HTTPException(status_code=404, detail="No se encontraron escuelas")
-
     return escuelas
 
 
 @app.get("/escuelas/{escuela_id}", response_model=EscuelaInDB)
 def read_escuela(escuela_id: int, db: Session = Depends(get_db)):
-    # Llamar al método CRUD para obtener la escuela por su ID
+    # Obtener: Llamar al método CRUD para obtener una escuela por su ID
     escuela = get_escuela_by_id(db, escuela_id)
 
-    # Si la escuela no existe, lanzar un error 404
     if escuela is None:
         raise HTTPException(status_code=404, detail="Escuela no encontrada")
 
@@ -119,13 +115,13 @@ def read_escuela(escuela_id: int, db: Session = Depends(get_db)):
 
 @app.post("/escuelas/", response_model=EscuelaInDB)
 def create_new_escuela(escuela: EscuelaCreate, db: Session = Depends(get_db)):
-    # Llamar al método CRUD para crear la escuela
+    # Crear: Crear una nueva escuela
     nueva_escuela = create_escuela(db, escuela)
-
     return nueva_escuela
 
 
 # Eliminar escuela por ID
 @app.delete("/escuelas/{escuela_id}")
 def delete_escuela(escuela_id: int, db: Session = Depends(get_db)):
+    # Eliminar: Eliminar una escuela por su ID
     return delete_escuela_by_id(db, escuela_id)
