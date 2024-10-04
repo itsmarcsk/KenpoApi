c17015642650be9b8c53e5bec3fb3cf7137d4b01
Mark <marcgimdi@gmail.com>
2024-10-04 16:53:35 +0200

    docker me da la vida

--- BBDD/mysql/database.py
+++ BBDD/mysql/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:root@localhost:3307/tfc"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:root@172.18.0.3:3306/tfc"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:root@localhost:3306/tfc"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:root@172.18.0.3:3306/tfc"
 
 
 # Crea el motor de la base de datos
